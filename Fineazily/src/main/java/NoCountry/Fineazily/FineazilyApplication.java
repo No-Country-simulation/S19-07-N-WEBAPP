049b9a7a036ba5fbde7b6ec3c11769f5fbad5aa1
leonls1 <leonlederhossturich@gmail.com>
2024-11-19 22:16:07 -0300

    structure for backEnd

---
+++ Fineazily/src/main/java/NoCountry/Fineazily/FineazilyApplication.java
@@ -0,0 +1,13 @@
+package NoCountry.Fineazily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FineazilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FineazilyApplication.class, args);
+	}
+
+}
