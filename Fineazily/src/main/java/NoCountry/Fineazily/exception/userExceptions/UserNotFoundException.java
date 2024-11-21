52add1e9ad5e1cffdeb5cf4ecc2146eb79df12da
leonls1 <leonlederhossturich@gmail.com>
2024-11-20 22:13:11 -0300

    added role entity

--- Fineazily/src/main/java/NoCountry/Fineazily/exception/userExceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package NoCountry.Fineazily.exception.userExceptions;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
