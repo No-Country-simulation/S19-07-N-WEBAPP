f6dafccfe65677c97f827a56d0b73f2eab6403c9
leonls1 <leonlederhossturich@gmail.com>
2024-11-20 21:30:16 -0300

    reset structure

--- Fineazily/src/main/java/NoCountry/Fineazily/model/dto/UserDto.java
+++
@@ -1,6 +0,0 @@
-package NoCountry.Fineazily.model.dto;
-
-public record UserDto(Long id,
-                      @
-                      String name, String email, String password, String roleName) {
-}
