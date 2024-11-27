48b563f08c040e4f9b889c711f05efe0a7c7339d
leonls1 <leonlederhossturich@gmail.com>
2024-11-27 11:16:12 -0300

    completed basic structure for box, solved problem with mapStruct at the time the entity is updated

--- Back/Fineazily/src/main/java/NoCountry/Fineazily/model/mapper/BranchMapper.java
+++ Back/Fineazily/src/main/java/NoCountry/Fineazily/model/mapper/BranchMapper.java
@@ -3,10 +3,13 @@ package NoCountry.Fineazily.model.mapper;
 import NoCountry.Fineazily.model.dto.BranchDTO;
 import NoCountry.Fineazily.model.entity.Branch;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface BranchMapper {
     Branch toEntity(BranchDTO dto);
 
     BranchDTO toDto(Branch branch);
+
+    void updateBranch(BranchDTO dto, @MappingTarget Branch branch);
 }
