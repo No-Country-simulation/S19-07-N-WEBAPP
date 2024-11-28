920532e022d2a7586c035114041d60e21489a81a
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2024-11-28 11:57:58 -0700

    fix(role): change role name to uppercase for consistency
    - Updated role names to uppercase (e.g., 'ADMIN' instead of 'admin') for consistency.

--- Back/Fineazily/src/main/resources/db/migration/V2__insert_roles.sql
+++ Back/Fineazily/src/main/resources/db/migration/V2__insert_roles.sql
@@ -1,4 +1,4 @@
 INSERT INTO roles (name, display_name, description)
 VALUES
-    ('admin', 'Administrador', 'Acceso completo para gestionar la aplicación, incluidos usuarios y configuraciones.'),
-    ('cashier', 'Cajero', 'Gestiona transacciones diarias y pagos de clientes.');
+    ('ADMIN', 'Administrador', 'Acceso completo para gestionar la aplicación, incluidos usuarios y configuraciones.'),
+    ('CASHIER', 'Cajero', 'Gestiona transacciones diarias y pagos de clientes.');
