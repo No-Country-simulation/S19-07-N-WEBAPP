895f607b57b8deb018f14a1f8a594463363be5d8
leonls1 <leonlederhossturich@gmail.com>
2024-12-05 17:27:56 -0300

    5/12

--- Back/Fineazily/src/main/java/NoCountry/Fineazily/repostory/TransactionRepository.java
+++ Back/Fineazily/src/main/java/NoCountry/Fineazily/repostory/TransactionRepository.java
@@ -3,5 +3,10 @@ package NoCountry.Fineazily.repostory;
 import NoCountry.Fineazily.model.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    BigDecimal getTotalIncome();
+
+    BigDecimal getTotal
 }
