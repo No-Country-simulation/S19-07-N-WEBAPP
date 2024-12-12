0834fc40bfbf894d781f937b30e7a78ba626e431
jerangel1 <jerangel1691@gmail.com>
2024-12-11 22:30:12 -0400

    Feat Metadata and openGraph

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -10,8 +10,44 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Financy",
-  description: "seleccionado 19 | grupo 7",
+  title: "Fineazily | Gestión Integral para PYMES",
+  description: "Herramienta all-in-one para administrar sucursales, personal y finanzas. Optimiza tu negocio con tecnología inteligente.",
+  keywords: [
+    "PYME", 
+    "gestión empresarial", 
+    "finanzas", 
+    "administración", 
+    "sucursales", 
+    "recursos humanos", 
+    "software de gestión"
+  ],
+  openGraph: {
+    title: "Fineazily - Solución Integral para tu Negocio",
+    description: "Transforma la gestión de tu empresa con nuestra plataforma todo en uno",
+    images: [
+      {
+        url: "https://raw.githubusercontent.com/No-Country-simulation/S19-07-N-WEBAPP/MS-A/front/public/FLogo.webp",
+        width: 1200,
+        height: 630,
+        alt: "Financy - Gestión Empresarial"
+      }
+    ],
+    type: "website",
+    locale: "es_ES"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Fineazily| Gestión Integral para PYMES",
+    description: "Herramienta all-in-one para administrar sucursales, personal y finanzas.",
+    images: [
+      "https://raw.githubusercontent.com/No-Country-simulation/S19-07-N-WEBAPP/MS-A/front/public/FLogo.webp"
+    ]
+  },
+  applicationName: "Fineazily",
+  authors: [{ name: "No Country Team", url: "https://www.nocountry.tech" }],
+  creator: "No Country Simulation Team equipo-s19-07-n-webapp ",
+  publisher: "No Country",
+  robots: "index, follow",
 };
 
 export default function RootLayout({
@@ -20,7 +56,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${poppins.variable} antialiased`}>
         <div className="flex min-h-screen overflow-hidden">
           <div className="flex-1">
