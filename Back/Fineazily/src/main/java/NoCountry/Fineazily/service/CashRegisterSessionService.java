8bd9d8433bfbafbb13dfb2dd41775bb475a4460c
leonls1 <leonlederhossturich@gmail.com>
2024-12-18 17:21:39 -0300

    18/12

--- Back/Fineazily/src/main/java/NoCountry/Fineazily/service/CashRegisterSessionService.java
+++ Back/Fineazily/src/main/java/NoCountry/Fineazily/service/CashRegisterSessionService.java
@@ -1,10 +1,21 @@
 package NoCountry.Fineazily.service;
 
 import NoCountry.Fineazily.model.entity.CashRegisterSession;
+import NoCountry.Fineazily.repostory.CashRegisterSessionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CashRegisterSessionService {
+    private final CashRegisterSessionRepository repository;
+    private final String notFound= "there isn't a session with that id:";
+    private final CashRegisterSessionMapper
+
+    public void createSession(SessionRequest){
+
+    }
+
     public CashRegisterSession findById(Long id){
         return null;
     }
